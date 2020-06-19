a4c3bad4cad8955a9a551816752174e8b6fa4f43
TRY0929 <1051977213@qq.com>
2020-06-19 10:30:14 +0800

    project init

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <!-- <img src="./assets/logo.png"> -->
+    <!-- 显示的是当前路由地址对应的内容 -->
     <router-view/>
   </div>
 </template>
@@ -12,12 +13,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
