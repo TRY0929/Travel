8c36c7eba5a0dc9dedf16ab6ba19f010ff64cca6
TRY0929 <1051977213@qq.com>
2020-06-24 15:36:58 +0800

    add alphabet to city

---
+++ src/pages/city/components/Search.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="search">
+    <input type="text" class="search-input" placeholder="输入城市名或拼音" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CitySearch'
+}
+</script>
+
+<style lang='stylus' scoped>
+  @import '~styles/variables.styl'
+ .search
+   height: .72rem
+   background-color: $bgColor
+   padding: 0 .1rem
+   .search-input
+     box-sizing: border-box
+     height: .62rem
+     line-height: .62rem
+     width: 100%
+     text-align: center
+     border-radius: .06rem
+     color: #ccc
+     padding: 0 .1rem
+</style>
